dda70513cae925e5e78519ef5a76e1f96bfdfab8
bailey168 <baileyng168@gmail.com>
2025-05-29 16:07:20 -0400

    Updated sleep time to 1h

--- sbatch_loop.sh
+++ sbatch_loop.sh
@@ -5,5 +5,5 @@ for path_file in /external/rprshnas01/tigrlab/scratch/bng/cartbind/code/MIND_mod
     max_index=$((num_lines - 1))
     sbatch --array=0-$max_index submit_MIND_merged_loop.sbatch "$path_file"
     echo "Submitted job for $path_file with array range 0-$max_index"
-    sleep 2h
+    sleep 1h
 done
