6991f7f03f4e44b9ab1e03824e891d77cb596cb8
bailey168 <baileyng168@gmail.com>
2025-05-29 12:28:06 -0400

    Created paths sublists and batching scripts

---
+++ sbatch_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for path_file in /external/rprshnas01/tigrlab/scratch/bng/cartbind/code/MIND_models/subject_paths/paths_part_*; do
+    num_lines=$(wc -l < "$path_file")
+    max_index=$((num_lines - 1))
+    sbatch --array=0-$max_index submit_MIND_merged_loop.sbatch "$path_file"
+    echo "Submitted job for $path_file with array range 0-$max_index"
+done
