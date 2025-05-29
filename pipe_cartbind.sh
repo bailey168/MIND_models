56057a007f529c49fc4ebcb8f67d559cf6ef6e8e
Bailey Ng <bng@scclogin01.camhres.ca>
2025-05-29 15:48:36 -0400

    Updated cartbind sbatch

--- pipe_cartbind.sh
+++ pipe_cartbind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-printf "%s\n" /KIMEL/tigrlab/projects/colin/CARTBIND_FMRIPrep/fmriprep_cartbind_cmh/freesurfer/sub*/ | while read -r dir; do
+printf "%s\n" /KIMEL/tigrlab/projects/colin/CARTBIND_FMRIPrep/fmriprep_cartbind_uhn/freesurfer/sub*/ | while read -r dir; do
     [ -d "$dir" ] && printf "%s\n" "$dir"
-done > /external/rprshnas01/tigrlab/scratch/bng/cartbind/code/paths_aparc_camh.loc
+done > /external/rprshnas01/tigrlab/scratch/bng/cartbind/code/paths_aparc_uhn.loc
