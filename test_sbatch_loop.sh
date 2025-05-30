b7d8ab20ae6978f4b075c2ebebc8adc7337e6829
bailey168 <baileyng168@gmail.com>
2025-05-30 13:56:20 -0400

    Created full loop scripts for processing MIND on SCC dev server

---
+++ test_sbatch_loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for path_file in /external/rprshnas01/tigrlab/scratch/bng/cartbind/code/MIND_models/subject_paths/paths_part_*; do
+    num_lines=$(wc -l < "$path_file")
+    max_index=$((num_lines - 1))
+    sbatch --array=0-$max_index test_submit_MIND_merged_loop.sbatch "$path_file"
+    echo "Submitted job for $path_file with array range 0-$max_index"
+    sleep 1h
+done
