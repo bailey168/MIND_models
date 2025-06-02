732749300710384cf370b4eb813b7a20713fa15d
bailey168 <baileyng168@gmail.com>
2025-06-02 15:09:27 -0400

    Added merged unprocessed paths

--- merge_loc.py
+++ merge_loc.py
@@ -3,7 +3,7 @@ with open('/Users/baileyng/MIND_models/paths_HCP-MMP.loc', 'r') as f:
     hcp_mmp_paths = set(line.strip() for line in f if line.strip())
 
 # Open the output file for writing
-with open('/Users/baileyng/MIND_models/paths_aparc.loc', 'r') as aparc, open('/Users/baileyng/MIND_models/paths_merged.loc', 'w') as out:
+with open('/Users/baileyng/MIND_models/unprocessed_aparc_paths.loc', 'r') as aparc, open('/Users/baileyng/MIND_models/unprocessed_paths_merged.loc', 'w') as out:
     for line in aparc:
         path = line.rstrip('\n')
         if path in hcp_mmp_paths:
