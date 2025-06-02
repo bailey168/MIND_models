1e0d1a2782e0754c37e160f52cc6faa0c156c209
bailey168 <baileyng168@gmail.com>
2025-06-02 14:33:37 -0400

    created find_unprocessed_paths.sh

---
+++ find_unprocessed_paths.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOC_FILE="$1"
+CSV_DIR="$2"
+OUTPUT_FILE="$3"
+
+while read -r path; do
+    path=${path%/}
+    id=$(basename "$path")
+
+    csv_file="${CSV_DIR}/${id}_aparc_MIND_matrix.csv"
+    if [[ ! -f "$csv_file" ]]; then
+        echo "$path" >> "$OUTPUT_FILE"
+    fi
+
+done < "$LOC_FILE"
+
+echo "Done! Unprocessed paths written to '$OUTPUT_FILE'."
