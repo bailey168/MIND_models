fc9058959bb4d2e8cdddea00f53c48e517b18d79
bailey168 <baileyng168@gmail.com>
2025-06-06 13:06:55 -0400

    Created average_matrix.py

--- explore_mind_matrix.py
+++ explore_mind_matrix.py
@@ -22,3 +22,24 @@ plt.xlabel("Region")
 plt.ylabel("Region")
 plt.tight_layout()
 plt.show()
+
+df = pd.read_csv("/Users/baileyng/MIND_data/MIND_results/row_averages.csv", index_col=0)
+
+print("Shape:", df.shape)
+# print("\nHead:\n", df.head())
+
+# Print all column names
+# print("\nColumn names:\n", list(df.columns))
+
+# Print all row names
+# print("\nRow names:\n", list(df.index))
+
+
+
+plt.figure(figsize=(8, 8))
+sns.heatmap(df, cmap="viridis", square=True, xticklabels=1, yticklabels=1)
+plt.title("MIND Matrix Heatmap")
+plt.xlabel("Region")
+plt.ylabel("Region")
+plt.tight_layout()
+plt.show()
