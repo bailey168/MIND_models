35afce43ce23633dc677d8f4a84d310abefcd7d4
bailey168 <baileyng168@gmail.com>
2025-06-06 14:31:05 -0400

    Created check_size_271.py

---
+++ check_size_271.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import glob
+import os
+
+input_dir = '/external/rprshnas01/tigrlab/scratch/bng/cartbind/data/test_results/HCP-MMP'
+
+for csv_file in glob.glob(os.path.join(input_dir, '*_MIND_matrix.csv')):
+    df_matrix = pd.read_csv(csv_file, index_col=0)
+    if df_matrix.shape != (271, 271):
+        print(f"File {os.path.basename(csv_file)} has shape {df_matrix.shape}, not (271, 271)")
+
+print("Done checking sizes of MIND matrices!")
