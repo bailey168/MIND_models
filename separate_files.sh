a5dbd2999c8011b56968aaff6d1f4556f33aa341
bailey168 <baileyng168@gmail.com>
2025-06-06 22:20:59 -0400

    updated separate_files.sh to loop through files individually

--- separate_files.sh
+++ separate_files.sh
@@ -8,7 +8,13 @@ TARGET_DIR_3="/external/rprshnas01/tigrlab/scratch/bng/cartbind/data/test_result
 mkdir -p "$TARGET_DIR_2"
 mkdir -p "$TARGET_DIR_3"
 
-cp "$SOURCE_DIR"/*2_0_aparc_MIND_matrix.csv "$TARGET_DIR_2"
-cp "$SOURCE_DIR"/*3_0_aparc_MIND_matrix.csv "$TARGET_DIR_3"
+for file in "$SOURCE_DIR"/*; do
+    filename=$(basename "$file")
+    if [[ "filename" == *2_0_aparc_MIND_matrix.csv ]]; then
+        cp "$file" "$TARGET_DIR_2"
+    elif [[ "filename" == *3_0_aparc_MIND_matrix.csv ]]; then
+        cp "$file" "$TARGET_DIR_3"
+    fi
+done
 
 echo "Files copied successfully!"
