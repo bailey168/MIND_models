1228eb9723e9e7b83da63b7aad840c45a2d21ea2
bailey168 <baileyng168@gmail.com>
2025-06-26 11:46:47 -0400

    Updated clean_csv.py

--- remove_outliers.py
+++ remove_outliers.py
@@ -1,9 +1,30 @@
 import pandas as pd
+import numpy as np
 
 input_path = '/Users/baileyng/MIND_data/ukb_FIS.csv'
 output_path = '/Users/baileyng/MIND_data/ukb_FIS_no_outliers.csv'
 
 df = pd.read_csv(input_path, index_col=0)
 
+print("Initial shape of DataFrame:", df.shape)
 
+exclude_cols = ['31-0.0', '54-2.0']
 
+continuous_cols = [col for col in df.select_dtypes(include=[np.number]).columns
+                   if col not in exclude_cols]
+
+means = df[continuous_cols].mean()
+stds = df[continuous_cols].std()
+
+outlier_mask = np.abs(df[continuous_cols] - means) > (5 * stds)
+outlier_counts = outlier_mask.sum(axis=1)
+
+# Threshold: at least 50% of columns
+threshold = len(continuous_cols) * 0.5
+
+# Keep rows with fewer than 50% outliers
+df_cleaned = df[outlier_counts < threshold]
+
+print("Shape of DataFrame without outliers:", df_cleaned.shape)
+
+df_cleaned.to_csv(output_path, index=False)
