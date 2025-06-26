4ab14f481f2fd388c64d682a3e53c93933a4aefb
bailey168 <baileyng168@gmail.com>
2025-06-26 11:06:25 -0400

    Added extract_tabular2.py

--- remove_outliers.py
+++ remove_outliers.py
@@ -1,1 +1,9 @@
 import pandas as pd
+
+input_path = '/Users/baileyng/MIND_data/ukb_FIS.csv'
+output_path = '/Users/baileyng/MIND_data/ukb_FIS_no_outliers.csv'
+
+df = pd.read_csv(input_path, index_col=0)
+
+
+
