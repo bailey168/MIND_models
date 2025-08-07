6fab13d07835ae1c48ccd54fb268a7412e7745b8
bailey168 <baileyng168@gmail.com>
2025-08-07 15:45:57 -0400

    updated

--- 3D+4D/check_size_68.py
+++ 3D+4D/check_size_68.py
@@ -2,11 +2,11 @@ import pandas as pd
 import glob
 import os
 
-input_dir = '/scratch/p/pz249/baileyng/data/MIND_results/full'
+input_dir = '/scratch/p/pz249/baileyng/3D+4D_data/MIND_results/full'
 
 for csv_file in glob.glob(os.path.join(input_dir, '*_MIND_matrix.csv')):
     df_matrix = pd.read_csv(csv_file, index_col=0)
     if df_matrix.shape != (68, 68):
         print(f"File {os.path.basename(csv_file)} has shape {df_matrix.shape}, not (68, 68)")
 
-print("Done checking sizes of MIND matrices!")
+print("Done checking sizes of MIND 3D+4D matrices!")
