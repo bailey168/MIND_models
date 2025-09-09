666af2630101b86de41f9bde8aa47166ce61a4c4
bailey168 <baileyng168@gmail.com>
2025-09-09 14:46:43 -0400

    Updated

--- QuantNets/gnn/architectures.py
+++ QuantNets/gnn/architectures.py
@@ -43,15 +43,23 @@ class GraphConvNet(torch.nn.Module):
                                     out_channels=4 * model_dim,
                                     bias=bias,
                                     aggr=aggr
+                                    )] + \
+                            [GraphConv(
+                                    in_channels=4 * model_dim,
+                                    out_channels=4 * model_dim,
+                                    bias=bias,
+                                    aggr=aggr
                                     )]
         self.conv_layers = torch.nn.ModuleList(self.conv_layers)
 
         # Add batch normalization and activation layers
         self.batch_norms = torch.nn.ModuleList([
             torch.nn.BatchNorm1d(1 * model_dim),
-            torch.nn.BatchNorm1d(2 * model_dim)
+            torch.nn.BatchNorm1d(2 * model_dim),
+            torch.nn.BatchNorm1d(4 * model_dim)
         ])
         self.activations = torch.nn.ModuleList([
+            torch.nn.LeakyReLU(),
             torch.nn.LeakyReLU(),
             torch.nn.LeakyReLU()
         ])
@@ -86,7 +94,120 @@ class GraphConvNet(torch.nn.Module):
 
             if i < self.layers_num - 1:
                 data.x = self.batch_norms[i](data.x)
-                # data.x = self.activations[i](data.x)
+                data.x = self.activations[i](data.x)
+
+        graph_features = global_mean_pool(data.x, data.batch)
+
+        # Process demographic features through linear layer and concatenate
+        if self.include_demo and hasattr(data, 'demographics'):
+            demo_features = self.demo_projection(data.demographics)
+            combined_features = torch.cat([graph_features, demo_features], dim=1)
+        else:
+            combined_features = graph_features
+
+        x = self.classifier(combined_features)
+
+        # Changed for regression:
+        if self.out_dim == 1:
+            return x.squeeze(-1)  # For single-output regression, return scalar values
+        else:
+            return x  # For multi-output regression, return raw values (no softmax)
+        
+
+# GATv2Conv 2021 https://arxiv.org/abs/2105.14491
+# class GATv2Conv(in_channels: Union[int, Tuple[int, int]], 
+#                 out_channels: int, 
+#                 heads: int = 1, 
+#                 concat: bool = True, 
+#                 negative_slope: float = 0.2, 
+#                 dropout: float = 0.0, 
+#                 add_self_loops: bool = True, 
+#                 edge_dim: Optional[int] = None, 
+#                 fill_value: Union[float, Tensor, str] = 'mean', 
+#                 bias: bool = True, 
+#                 share_weights: bool = False)
+class GATv2ConvNet(torch.nn.Module):
+    def __init__(self, out_dim, input_features, output_channels, layers_num, 
+                model_dim, hidden_sf=4, out_sf=2, hidden_heads=4, bias=True, aggr='add',
+                embedding_dim=16, include_demo=True, demo_dim=4):
+        super(GATv2ConvNet, self).__init__()
+        self.layers_num = layers_num
+        self.out_dim = out_dim  # Store output dimension
+        self.include_demo = include_demo
+        self.demo_dim = demo_dim
+
+        self.node_embedding = torch.nn.Embedding(
+            num_embeddings=input_features,
+            embedding_dim=embedding_dim
+        )
+
+        self.conv_layers = [GATv2Conv(
+                                    in_channels=embedding_dim,
+                                    out_channels=16,
+                                    heads=hidden_heads,
+                                    bias=bias,
+                                    edge_dim=1,
+                                    residual=True
+                                    )] + \
+                           [GATv2Conv(
+                                    in_channels=64,
+                                    out_channels=16,
+                                    heads=hidden_heads,
+                                    bias=bias,
+                                    edge_dim=1,
+                                    residual=True
+                                    )] + \
+                           [GATv2Conv(
+                                    in_channels=64,
+                                    out_channels=16,
+                                    heads=hidden_heads,
+                                    bias=bias,
+                                    edge_dim=1,
+                                    residual=True
+                                    )]
+        self.conv_layers = torch.nn.ModuleList(self.conv_layers)
+
+        # # Add batch normalization and activation layers
+        # self.batch_norms = torch.nn.ModuleList([
+        #     torch.nn.BatchNorm1d(1 * model_dim),
+        #     torch.nn.BatchNorm1d(2 * model_dim)
+        # ])
+        # self.activations = torch.nn.ModuleList([
+        #     torch.nn.LeakyReLU(),
+        #     torch.nn.LeakyReLU()
+        # ])
+
+        # Calculate final feature dimension
+        graph_features_dim = 64
+
+        if self.include_demo:
+            self.demo_projection = torch.nn.Linear(self.demo_dim, 16)
+            total_features_dim = graph_features_dim + 16
+        else:
+            total_features_dim = graph_features_dim
+
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(total_features_dim, 64),
+            torch.nn.BatchNorm1d(64),
+            torch.nn.LeakyReLU(),
+            torch.nn.Dropout(0.1),
+            torch.nn.Linear(64, 32),
+            torch.nn.BatchNorm1d(32),
+            torch.nn.LeakyReLU(),
+            torch.nn.Dropout(0.1),
+            torch.nn.Linear(32, out_dim)
+        )
+
+    def forward(self, data):
+        data.x = self.node_embedding(data.x)
+
+        for i in range(self.layers_num):
+            edge_attr = data.edge_attr
+            data.x = self.conv_layers[i](data.x, data.edge_index, edge_attr=edge_attr)
+
+            # if i < self.layers_num - 1:
+            #     data.x = self.batch_norms[i](data.x)
+            #     data.x = self.activations[i](data.x)
 
         graph_features = global_mean_pool(data.x, data.batch)
 
