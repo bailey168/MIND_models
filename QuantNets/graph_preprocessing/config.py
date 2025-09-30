9fcdf5323201ce17ce522bed44c9ad970a0ccc79
bailey168 <baileyng168@gmail.com>
2025-09-30 16:12:05 -0400

    Updated to process DSST100

--- QuantNets/graph_preprocessing/config.py
+++ QuantNets/graph_preprocessing/config.py
@@ -17,7 +17,7 @@ EID_COL = "eid"
 # Parameters
 TRAIN_RATIO = 0.8
 RANDOM_SEED = 42
-KEEP_PERCENT = 0.30
+KEEP_PERCENT = 1.0
 KEEP_SELF_LOOPS = True
 
 # Standardization control
@@ -26,7 +26,7 @@ USE_EDGE_STANDARDIZATION = True
 # Dynamic paths based on parameters
 EDGE_PERCENT_STR = str(int(KEEP_PERCENT * 100))
 GRAPH_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/raw/{DATASET}{EDGE_PERCENT_STR}")
-PROCESSED_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/processed/{DATASET}{EDGE_PERCENT_STR}")  # Add this
+PROCESSED_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/processed/{DATASET}{EDGE_PERCENT_STR}")
 
 # Dynamic dataset name
 DATASET_NAME = f"custom_dataset_selfloops_{KEEP_SELF_LOOPS}_edgeft_None_norm_{USE_EDGE_STANDARDIZATION}"
