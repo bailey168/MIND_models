561d66aed91ebc90e8bcd92c043bdf3b37c3e456
bailey168 <baileyng168@gmail.com>
2025-09-30 15:14:48 -0400

    Updated config to progress DSST30 graphs

--- QuantNets/graph_preprocessing/config.py
+++ QuantNets/graph_preprocessing/config.py
@@ -3,9 +3,9 @@ import os
 # Paths
 DATA_DIR = "/scratch/bng/cartbind/data"
 OUTPUT_DIR = "/scratch/bng/cartbind/data"
-CSV_FILE = os.path.join(DATA_DIR, "ukb_master_GF_no_outliers.csv")
-TRAINING_IDS_FILE = os.path.join(OUTPUT_DIR, "training_ids/GF.txt")
-MATRIX_DIR = os.path.join(DATA_DIR, "FC_matrices/GF")
+CSV_FILE = os.path.join(DATA_DIR, "ukb_master_DSST_no_outliers.csv")
+TRAINING_IDS_FILE = os.path.join(OUTPUT_DIR, "training_ids/DSST.txt")
+MATRIX_DIR = os.path.join(DATA_DIR, "FC_matrices/DSST")
 
 # Column names
 DATA_COL = "20016-2.0"
@@ -22,8 +22,8 @@ USE_EDGE_STANDARDIZATION = True
 
 # Dynamic paths based on parameters
 EDGE_PERCENT_STR = str(int(KEEP_PERCENT * 100))
-GRAPH_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/raw/GF{EDGE_PERCENT_STR}")
-PROCESSED_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/processed/GF{EDGE_PERCENT_STR}")  # Add this
+GRAPH_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/raw/DSST{EDGE_PERCENT_STR}")
+PROCESSED_DIR = os.path.join(OUTPUT_DIR, f"FC_graphs/processed/DSST{EDGE_PERCENT_STR}")  # Add this
 
 # Dynamic dataset name
 DATASET_NAME = f"custom_dataset_selfloops_{KEEP_SELF_LOOPS}_edgeft_None_norm_{USE_EDGE_STANDARDIZATION}"
